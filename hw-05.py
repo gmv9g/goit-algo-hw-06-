98e87da299921e9a5adda04db76b73408c6cbb7a
gmv9g <diddid21222@gmail.com>
2024-07-16 20:23:02 +0300

    update hw-05

--- hw-05.py
+++ hw-05.py
@@ -44,3 +44,73 @@ def sum_profit(text: str, func: Callable):
 text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 37.45 і 424.00 доларів."
 total_income = sum_profit(text, generator_numbers)
 print(f"Загальний дохід: {total_income}")
+
+
+# 4 завдання 
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Введіть ім'я та номер телефону, будь ласка."
+        except KeyError:
+            return "Неправильна команда або ім'я контакту."
+        except IndexError:
+            return "Неправильна кількість аргументів."
+    return inner
+
+contacts = {}
+
+@input_error
+def add_contact(args):
+    name, phone = args
+    contacts[name] = phone
+    return "Контакт додано."
+
+@input_error
+def find_phone(name):
+    if name in contacts:
+        return f"{name}: {contacts[name]}"
+    else:
+        return f"Контакт '{name}' не знайдено."
+
+@input_error
+def list_all_contacts():
+    if contacts:
+        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+    else:
+        return "Контакти не знайдено."
+
+def main():
+    while True:
+        command = input("Введіть команду: ").strip().lower()
+
+        if command.startswith("add"):
+            try:
+                _, arg = command.split(maxsplit=1)
+                result = add_contact(arg.split())
+                print(result)
+            except Exception as e:
+                print(f"Сталася помилка: {e}")
+        
+        elif command.startswith("phone"):
+            try:
+                _, arg = command.split(maxsplit=1)
+                result = find_phone(arg.strip())
+                print(result)
+            except Exception as e:
+                print(f"Сталася помилка: {e}")
+
+        elif command.startswith("all"):
+            try:
+                result = list_all_contacts()
+                print(result)
+            except Exception as e:
+                print(f"Сталася помилка: {e}")
+        
+        else:
+            print("Неправильна команда.")
+
+if __name__ == "__main__":
+    main()
